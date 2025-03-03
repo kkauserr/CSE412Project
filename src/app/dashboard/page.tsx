a210a955cefb70b660ea8854d7998646354eccc5
Amogh Shetty <amoggo2004@gmail.com>
2025-03-02 23:45:48 -0700

    Edit dashboard page text

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default async function Dashboard() {
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold">Welcome, {session.user?.email}!</h1>
-      <p>This is your dashboard.</p>
+      <p>This is the dashboard WORK IN PROGRESS</p>
     </div>
   );
 }
