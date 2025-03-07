cff32044cdb41dfbc20e498ce85ed4172aff6484
Amogh Shetty <amoggo2004@gmail.com>
2025-03-06 18:10:00 -0700

    Change dashboard  text from User's email to name

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,8 +9,8 @@ export default async function Dashboard() {
   }
 
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold">Welcome, {session.user?.email}!</h1>
+    <div className="p-6 mt-20">
+      <h1 className="text-3xl font-bold">Welcome, {session.user?.name}!</h1>
       <p>This is the dashboard WORK IN PROGRESS</p>
     </div>
   );
